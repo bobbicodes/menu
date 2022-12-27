f1d6eacf2f6b78cee2ff8b777c0a26014e4e14eb
Bobby Towers <porkostomus@mail.com>
2022-12-26 23:07:40 -0800

    import image

---
+++ src/main/menu/app.cljs
@@ -0,0 +1,45 @@
+(ns menu.app
+  (:require [reagent.dom :as rdom]
+            [reagent.core :as r]
+            [goog.object :as o]))
+
+(defonce file-atom (r/atom nil))
+
+(defn import-image []
+  [:div
+   [:h1 "Import image"]
+   [:input#input
+    {:type      "file"
+     :on-change
+     (fn [e]
+       (let [dom    (o/get e "target")
+             file   (o/getValueByKeys dom #js ["files" 0])
+             reader (js/FileReader.)]
+         (.readAsDataURL reader file)
+         (set! (.-onload reader)
+               #(reset! file-atom (-> % .-target .-result)))))}]])
+
+@file-atom
+
+(defn app []
+  [:div#app
+   [import-image]
+   
+   [:img {:src   @file-atom
+          :width 400
+          }]])
+
+(defn render []
+  (rdom/render [app]
+            (.getElementById js/document "root")))
+
+(defn ^:dev/after-load start []
+  (render)
+  (js/console.log "start"))
+
+(defn ^:export init []
+  (js/console.log "init")
+  (start))
+
+(defn ^:dev/before-load stop []
+  (js/console.log "stop"))
---
+++ src/test/menu/app_test.cljs
@@ -0,0 +1,10 @@
+(ns menu.app-test
+  (:require [cljs.test :refer [deftest is run-tests]]
+            [menu.app :as app]))
+
+(deftest my-test
+  )
+
+(comment
+  (run-tests)
+  )
