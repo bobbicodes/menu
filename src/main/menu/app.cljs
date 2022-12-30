fd612b76468017106fa66f1c87024b64273e732f
Bobby Towers <porkostomus@mail.com>
2022-12-30 07:10:22 -0800

    add more images

--- src/main/menu/app.cljs
+++ src/main/menu/app.cljs
@@ -4,6 +4,45 @@
             [goog.object :as o]))
 
 (defonce !files (r/atom []))
+(defonce !bg (r/atom nil))
+(defonce posX (r/atom 0))
+(defonce dom-node (r/atom nil))
+(def w (.-innerWidth js/window))
+(def h (.-innerHeight js/window))
+(def image (js/Image.))
+
+(defn draw []
+  (let [canvas (.-firstChild @dom-node)
+        ctx    (.getContext canvas "2d")]
+    (set! (.-width canvas) w)
+    (set! (.-height canvas) h)
+    (.fillRect ctx (- (/ w 2)) (- (/ h 2)) w h)
+    (.drawImage ctx image 0 0 
+                (.-width image) (.-height image)
+                @posX -74 
+                (/ (.-width image) 2) (/ (.-height image) 2))
+    (.requestAnimationFrame js/window draw)))
+
+(defn draw-canvas-contents [canvas]
+    (set! (.-src image) (first @!files))
+    (set! (.-onload image) draw))
+
+(defn div-with-canvas []
+    (r/create-class
+     {:component-did-update
+      (fn [this]
+        (draw-canvas-contents (.-firstChild @dom-node)))
+
+      :component-did-mount
+      (fn [this]
+        (reset! dom-node (rdom/dom-node this)))
+
+      :reagent-render
+      (fn []
+        [:div.with-canvas
+         [:canvas (if-let [node @dom-node]
+                    {:width w
+                     :height h})]])}))
 
 (defn upload-images [e]
     (let [dom    (o/get e "target")
@@ -15,6 +54,23 @@
           (set! (.-onload reader)
                 #(swap! !files conj (-> % .-target .-result)))))))
 
+(defn upload-bg [e]
+  (let [dom    (o/get e "target")
+        files (o/get dom "files")]
+    (reset! !files [])
+    (doseq [file files]
+      (let [reader (js/FileReader.)]
+        (.readAsDataURL reader file)
+        (set! (.-onload reader)
+              #(reset! !bg (-> % .-target .-result)))))))
+
+(defn import-bg []
+  [:div
+   [:h1 "Select background"]
+   [:input#bg
+    {:type      "file"
+     :on-change upload-bg}]])
+
 (defn import-images []
   [:div
    [:h1 "Import images"]
@@ -24,15 +80,20 @@
      :on-change upload-images}]])
 
 (defn images [files]
-  (into [:div#images]
+  (into [:div.flex-container]
         (for [file files]
-          [:img {:src   file
-                 :width 400 :height 400}])))
+          [:img.flex-item {:src   file
+                 :width 400 :height 400
+                 }])))
 
 (defn app []
-  [:div#app
-   [import-images]
-   [images @!files]])
+   [:div#app
+     {:style {:background-image (str "url(" @!bg ")")}}
+    [import-bg]
+    [import-images]
+    
+    [images @!files]
+    [div-with-canvas]])
 
 (defn render []
   (rdom/render [app]
