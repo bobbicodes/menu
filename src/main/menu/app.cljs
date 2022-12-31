839ec91580d8dbe9c60a0a05b4d2464afb8d6fc9
Bobby Towers <porkostomus@mail.com>
2022-12-30 21:34:48 -0800

    make transparent

--- src/main/menu/app.cljs
+++ src/main/menu/app.cljs
@@ -45,8 +45,58 @@
   (into [:div.flex-container]
         (for [file files]
           [:img.flex-item {:src   file
-                 :width 200 :height 200
-                 }])))
+                 :width 200 :height 200}])))
+
+
+;(def url (first @!files))
+;(def img (js/Image.))
+;(set! (.-src img) url)
+;(def canvas (.getElementById js/document "canvas"))
+;(def ctx    (.getContext canvas "2d"))
+;(set! (.-onload img) (.drawImage ctx img 0 0))
+
+(defn original [] 
+  (let [url (first @!files)
+        img (js/Image.)
+        _ (set! (.-src img) url)
+        canvas (.getElementById js/document "canvas")
+        ctx    (.getContext canvas "2d")
+        _ (set! (.-onload img) (.drawImage ctx img 0 0))]
+    (.drawImage ctx img 0 0)))
+
+(defn transparent [r g b]
+  (let [url (first @!files)
+        img (js/Image.)
+        _ (set! (.-src img) url)
+        canvas (.getElementById js/document "canvas")
+        ctx    (.getContext canvas "2d")
+        _ (set! (.-onload img) (.drawImage ctx img 0 0))
+        _  (.drawImage ctx img 0 0)
+        imageData (.getImageData ctx 0 0 (.-width canvas) (.-height canvas))
+        data (.-data imageData)]
+    (doseq [i (filter #(and
+                        (= r (aget data (- % 3)))
+                        (= g (aget data (- % 2)))
+                        (= b (aget data (- % 1))))
+                      (range 3 (.-length data) 4))]
+      (aset data i 0))
+    (.putImageData ctx imageData 0 0)))
+
+(let [url (first @!files)
+      img (js/Image.)
+      _ (set! (.-src img) url)
+      canvas (.getElementById js/document "canvas")
+      ctx    (.getContext canvas "2d")
+      _ (set! (.-onload img) (.drawImage ctx img 0 0))
+      _  (.drawImage ctx img 0 0)
+      imageData (.getImageData ctx 0 0 (.-width canvas) (.-height canvas))
+      data (.-data imageData)]
+  (take 50 data))
+
+
+(set! (.. js/document -body -style -backgroundImage) (str "url(" @!bg ")"))
+
+(transparent 255 255 255)
 
 (defn app []
    [:div#app
