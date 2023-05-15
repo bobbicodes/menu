8ed534878eea52f09e835dde49b91d6492c371f1
Bobby Towers <porkostomus@mail.com>
2023-05-15 10:41:28 -0700

    output product map with variants

--- hh.clj
+++ hh.clj
@@ -9,5 +9,11 @@
   (filter #(= s (get % "name")) products))
 
 (comment
-  (product "Tie Dye Tee")
+  (map #(get % "retail_price") (product "Tie Dye Tee"))
   )
+
+(for [i (range (count products))]
+  {:n i 
+   :name (get (nth products i) "name")
+   :price (get (nth products i) "retail_price")
+   :variant (get (nth products i) "variant_option_one_value")})
